0f1623d3ec8e2092eb8507318e77cd254a4e6110
joshk622 <joshk0622>
2022-12-01 17:51:25 +0800

    Fix Real Estate bug

--- qq news crawler/main.py
+++ qq news crawler/main.py
@@ -2,39 +2,45 @@ import qq_crawler_model
 import json
 import datetime
 import os
-fileds = {'news': "/html/body/div[1]/div[3]/div/ul/li[1]/a",
-          'financial': "/html/body/div[1]/div[3]/div/ul/li[5]/a",
-          'technology': "/html/body/div[1]/div[3]/div/ul/li[6]/a",
-          'fashion': "/html/body/div[1]/div[3]/div/ul/li[14]/a",
-          'car': "/html/body/div[1]/div[3]/div/ul/li[12]/a",
-          'real estate': "/html/body/div[1]/div[3]/div/ul/li[13]/a",
-          'financial management': "/html/body/div[1]/div[3]/div/div/div[2]/ul/li[5]/a",
-          'security': "/html/body/div[1]/div[3]/div/div/div[2]/ul/li[7]/a",
-          'digital': "/html/body/div[1]/div[3]/div/div/div[2]/ul/li[9]/a"
-          }
+fileds = {
+    'real estate': "/html/body/div[1]/div[3]/div/ul/li[13]/a",
+    'financial': "/html/body/div[1]/div[3]/div/ul/li[5]/a",
+    'news': "/html/body/div[1]/div[3]/div/ul/li[1]/a",
+    'technology': "/html/body/div[1]/div[3]/div/ul/li[6]/a",
+    'fashion': "/html/body/div[1]/div[3]/div/ul/li[14]/a",
+    'car': "/html/body/div[1]/div[3]/div/ul/li[12]/a",
+    'financial management': "/html/body/div[1]/div[3]/div/div/div[2]/ul/li[5]/a",
+    'security': "/html/body/div[1]/div[3]/div/div/div[2]/ul/li[7]/a",
+    'digital': "/html/body/div[1]/div[3]/div/div/div[2]/ul/li[9]/a",
+}
 
 if __name__ == "__main__":
     current_time = datetime.datetime.now()
     for field, xp in fileds.items():
-        model = qq_crawler_model.qq_model(xp, field)
-        result = model.run()
-        result = result.to_dict()
-        size = len(result)
-        count = 0
-        folder = os.path.exists(field)
-        print(result)
-        if not folder:
-            os.makedirs(field)
-            print("New folder for", field, "made")
+        if field != 'real estate':
+            model = qq_crawler_model.qq_model(xp, field)
+            result = model.run()
+            result = result.to_dict()
+            size = len(result)
+            count = 0
+            folder = os.path.exists(field)
+            print(result)
+            if not folder:
+                os.makedirs(field)
+                print("New folder for", field, "made")
+            else:
+                print("Saving data into", field, "folder")
+            for idx, data in result.items():
+                count += 1
+                res = dict.fromkeys((idx,), data)
+                jsonOrderedFile = json.dumps(res, indent=4, ensure_ascii=False)
+                filename = field + "/" + field + \
+                    "_" + str(idx).zfill(3) + ".json"
+                with open(filename, 'w', encoding='utf-8') as jsonfile:
+                    jsonfile.write(jsonOrderedFile)
+            model.browser.quit()
         else:
-            print("Saving data into", field, "folder")
-        for idx, data in result.items():
-            count += 1
-            res = dict.fromkeys((idx,), data)
-            jsonOrderedFile = json.dumps(res, indent=4, ensure_ascii=False)
-            filename = field + "/" + field + "_" + str(idx).zfill(3) + ".json"
-            with open(filename, 'w', encoding='utf-8') as jsonfile:
-                jsonfile.write(jsonOrderedFile)
-        model.browser.close()
+            estate_model = qq_crawler_model.estate_model()
+            estate_model.run()
     end_time = datetime.datetime.now()
     print("Running Time: ", str(end_time - current_time))
--- qq news crawler/qq_crawler_model.py
+++ qq news crawler/qq_crawler_model.py
@@ -7,6 +7,9 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 import numpy as np
 import time
+import os
+import json
+
 s = Service(
     'C:\Program Files\Google\Chrome\Application\chromedriver_win32\chromedriver.exe')
 more_xpath = "/html/body/div[1]/div[3]/div/div/div[1]"
@@ -29,8 +32,9 @@ class qq_model():
         self.scroll_to_bottom()
 
     def get_links(self):
+
         xpath = '//li[@class = "item cf itme-ls"]\
-                //div[@class = "detail"]//h3//a'
+                    //div[@class = "detail"]//h3//a'
         if self.xpath_exist(xpath) == False:
             return np.NAN
         links = self.browser.find_elements(By.XPATH, xpath)
@@ -163,3 +167,110 @@ class qq_model():
         result = pd.DataFrame({"Tag": self.field, "Id": Id, "Url": Url, "Title": Title, "Author": Author, "Publish_Date": Publish_Date,
                                "Post_time": Post_time, "Content": Content, "Recommendation_links": Recommendation_links})
         return result.transpose()  # return the Pandas DataFrame
+
+
+class estate_model():
+    def __init__(self):
+        self.browser = webdriver.Chrome(service=s)
+
+    def get_links(self):
+        self.browser.get("https://house.qq.com/")
+        time.sleep(1)
+        self.scroll_to_bottom()
+        time.sleep(1)
+        xpath = "//ul[@class='arealist area2']//li//div//h3//a"
+        if self.xpath_exist(xpath) == False:
+            return np.NAN
+        arr = []
+        for i in range(1, 5):
+            l_xpath = '/html/body/div[2]/div[5]/div[1]/div[3]/ul/li[' + \
+                str(i) + ']'
+            self.browser.find_element(By.XPATH, l_xpath).click()
+            links = self.browser.find_elements(By.XPATH, xpath)
+            for link in links:
+                arr.append(link.get_attribute('href'))
+        return pd.Series(arr)
+
+    def enter_art_site(self, links):
+        folder = os.path.exists('real estate')
+        if not folder:
+            os.makedirs('real estate')
+            print("New folder for real estate made")
+        else:
+            print("Saving data into real estate folder")
+        count = 0
+        size = len(links)
+
+        for link in links:
+            self.browser.get(link)
+            self.scroll_to_bottom()
+            id = link.split('/')[-1].split('.')[0]
+            date = id[:4] + '-' + id[4:6] + '-' + id[6:8]
+            ex_links = self.get_external_links()
+            ex_links = [x for x in ex_links if x !=
+                        None] if ex_links != np.NaN else np.NaN
+            content = self.get_content()
+            tag = 'real estate'
+            post_time = self.get_post_time()
+            d = dict(zip(['Tag', 'ID', 'Publish Date', 'Post Time', 'Content', 'Recommendation_links'], [
+                tag, id, date, post_time, content, ex_links]))
+            count += 1
+            print("Field: Real Estate", "\nProgress: ", count, "/", size)
+            jsonOrderedFile = json.dumps(d, indent=4, ensure_ascii=False)
+            filename = 'real estate' + "/" + 'Real_Estate' + \
+                "_" + str(count).zfill(3) + ".json"
+            with open(filename, 'w', encoding='utf-8') as jsonfile:
+                jsonfile.write(jsonOrderedFile)
+        self.browser.quit()
+
+    def scroll_to_bottom(self):
+        temp_height = 0
+        while True:
+            self.browser.execute_script("window.scrollBy(0, 10000)")
+            time.sleep(1.5)
+            check_height = self.browser.execute_script(
+                "return document.documentElement.scrollTop || \
+                window.pageYOffset || document.body.scrollTop;")
+            if check_height == temp_height:
+                break
+            temp_height = check_height
+
+    def get_external_links(self):
+        xpath = '//*[@class = "detail"]//h3//a'
+        if self.xpath_exist(xpath) == False:
+            return np.NaN
+        ex_links = self.browser.find_elements(By.XPATH, xpath)
+        arr = []
+        for ex_link in ex_links:
+            arr.append(ex_link.get_attribute('href'))
+        return arr
+
+    def get_content(self):
+        xpath = '//div[@class = "content-article"]//p[@class = "one-p"]'
+        if self.xpath_exist(xpath) == False:
+            return np.NAN
+        content = self.browser.find_elements(By.XPATH, xpath)
+        string = ""
+        for cont in content:
+            string += cont.text
+            string += '\n'
+        return string
+
+    def get_post_time(self):
+        xpath = '//*[@id="LeftTool"]/div/div[3]'
+        if self.xpath_exist(xpath) == False:
+            return np.NAN
+        post_time = self.browser.find_element(By.XPATH, xpath)
+        post_time = post_time.get_attribute("textContent")
+        return post_time
+
+    def run(self):
+        links = self.get_links()
+        self.enter_art_site(links)
+
+    def xpath_exist(self, xpath):
+        try:
+            self.browser.find_element(By.XPATH, xpath)
+            return True
+        except:
+            return False
